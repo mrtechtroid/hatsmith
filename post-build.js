fd1b477bad3819e311852bc66dc0df314c97e864
Voornaamenachternaam <84537103+Voornaamenachternaam@users.noreply.github.com>
2025-10-06 17:06:45 +0200

    Update post-build.js

--- post-build.js
+++ post-build.js
@@ -1,13 +1,21 @@
-const {
-  writeFile,
-  readFile,
-} = require("fs").promises;
+const { writeFile, readFile } = require("fs").promises;
 const { join } = require("path");
 
 const main = async () => {
-  const file = await readFile(join(__dirname, "out/404/index.html"));
+  console.log("Node version:", process.version);
+  console.log("NPM version:", (() => {
+    try {
+      return require("child_process").execSync("npm -v").toString().trim();
+    } catch {
+      return "unknown";
+    }
+  })());
 
+  const file = await readFile(join(__dirname, "out/404/index.html"));
   await writeFile(join(__dirname, "out/404.html"), file);
 };
 
-main();
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
