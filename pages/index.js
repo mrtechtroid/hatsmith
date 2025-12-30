86164f33dba58bdda1f4cd5cdd4db0076fa61a77
Voornaamenachternaam <84537103+Voornaamenachternaam@users.noreply.github.com>
2025-12-30 15:35:26 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ const Home = () => {
     //register service worker
     if ("serviceWorker" in navigator) {
       navigator.serviceWorker
-        .register("service-worker.js")
+        .register("/service-worker.js")
         .then((reg) => {
           reg.update();
           setSwReg(true);
